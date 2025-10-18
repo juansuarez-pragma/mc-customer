aeb608d0f49604688309a1102624f84f3346ec24
Juan Carlos Suarez Marin <juancarlossuarezmarin@MacBook-Pro-de-Juan.local>
2025-10-18 12:01:23 -0500

    initial project

---
+++ src/main/java/com/stech/mcc_customer_service/MccCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stech.mcc_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccCustomerServiceApplication.class, args);
+	}
+
+}
